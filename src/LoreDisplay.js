572ffa187d7eea656f598652de0f12c4d154fa4a
Krzysztof Kułak <krzysztofpiotrkulak@gmail.com>
2023-09-14 20:11:23 +0200

    improve scrolling

--- src/LoreDisplay.js
+++ src/LoreDisplay.js
@@ -4,6 +4,21 @@ function LoreDisplay({ lores, displayIndex, setDisplayIndex }) {
     const [animationPhase, setAnimationPhase] = useState('idle'); // 'idle', 'exiting', 'entering'
     const [displayedText, setDisplayedText] = useState('');
     
+    const animation_duration = 1000  // Assuming the enter animation duration is 1s
+
+    useEffect(() => {
+        if (lores.length > 0) {
+            setDisplayedText(lores[0]?.content);
+            setAnimationPhase('entering');
+        }
+    }, [lores]);
+
+    useEffect(() => {
+        if (animationPhase === 'idle') {
+            setDisplayedText(lores[displayIndex]?.content);
+        }
+    }, [displayIndex, lores]);
+    
     useEffect(() => {
         if (animationPhase === 'idle') {
             setDisplayedText(lores[displayIndex]?.content);
@@ -13,26 +28,42 @@ function LoreDisplay({ lores, displayIndex, setDisplayIndex }) {
     useEffect(() => {
         if (animationPhase === 'exiting') {
             const nextIndex = (displayIndex + 1) % lores.length;
-            const timer = setTimeout(() => {
-                setDisplayedText(lores[nextIndex]?.content);
+            let timer = setTimeout(() => {
                 setDisplayIndex(nextIndex);
                 setAnimationPhase('entering');
-            }, 1000); // Assuming the exit animation duration is 1s
-            
+                setDisplayedText(lores[nextIndex]?.content);
+            }, animation_duration);
             return () => clearTimeout(timer);
         }
     }, [animationPhase, displayIndex, lores]);
 
+    useEffect(() => {
+        if (animationPhase === 'entering') {
+            const timer = setTimeout(() => {
+                setAnimationPhase('idle');
+            }, animation_duration);
+
+            return () => clearTimeout(timer);
+        }
+    }, [animationPhase]);
+
     return (
-        <div className="skyrimLoading">
+        <div 
+            className="skyrimLoading"
+            onClick={() => {
+                if (animationPhase === 'idle') {
+                    setAnimationPhase('exiting');
+                }
+            }}
+        >
             <p 
                 className={`skyrimText ${animationPhase}`} 
-                onClick={() => setAnimationPhase(animationPhase === 'idle' ? 'exiting' : 'idle')}
             >
                 {displayedText}
             </p>
         </div>
     );
 }
+
 
 export default LoreDisplay;
